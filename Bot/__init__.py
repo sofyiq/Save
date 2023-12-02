05fa2440a0d4708412942bdc0390d187417ae37e
sofyiq <150921026+sofyiq@users.noreply.github.com>
2023-12-02 11:56:43 +0300

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,6 +6,5 @@ app = Client(
     api_id=26384753,
     api_hash="d0df15edaf47d46b36747f8af2e11b6f",
     bot_token="5249469006:AAEyC4hMWvcrZ8dTfY14dd_QTh7sqfroBO4",
-    in_memory=True,
     plugins=dict(root="Bot/handlers")
 )
