38584789b08391e0ef1bbe8b09e53cc150d27b34
sofyiq <150921026+sofyiq@users.noreply.github.com>
2023-12-02 11:51:31 +0300

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,16 +1,12 @@
 from pyrogram import Client
 import pyromod
 
-api_id = 26384753
-api_hash = "d0df15edaf47d46b36747f8af2e11b6f"
-bot_token = "5249469006:AAEyC4hMWvcrZ8dTfY14dd_QTh7sqfroBO4"
-
-
 app = Client(
-    session_name="name", 
-    api_id = api_id, 
-    api_hash = api_hash, 
-    bot_token = bot_token,
-    in_memory=True, 
+    "MediaDownloader",
+    26384753,
+    "d0df15edaf47d46b36747f8af2e11b6f",
+    bot_token="5249469006:AAEyC4hMWvcrZ8dTfY14dd_QTh7sqfroBO4",
+    in_memory=True
     plugins=dict(root="Bot/handlers")
 )
+
